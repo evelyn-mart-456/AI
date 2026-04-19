6cc84aa41f4b63d5db0fdd5dfa6643d3d175ca18
evelyn-mart-456 <martinez.e37@mymail.sxu.edu>
2026-04-19 12:33:46 -0500

    fix CORS issue

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+app.use(cors({
+    origin: "https://evelyn-mart-456.github.io"
+}));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
